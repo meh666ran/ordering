d9ee3c6c973f33218be7e48882294d55efa94e4c
Mehran <mehranzolghadr666@gmail.com>
2019-06-05 23:06:01 +0430

    clean project

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,3 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::prefix('admin')->group( function(){
-  Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login.form');
-  Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-  Route::get('/', 'AdminController@index')->name('admin.dashboard');
-} );
+// there's no web routes :)
