5fcefca58d53923608fac8ae49038dd38f4661fd
Mehran <mehranzolghadr666@gmail.com>
2019-07-07 20:32:46 +0430

    add cake needed fields

---
+++ database/migrations/2019_07_07_154924_create_cakes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCakesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cakes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('price');
+            $table->string('main_category');
+            $table->string('sub_category');
+            $table->string('weights');
+            $table->bigInteger('number_of_sells');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cakes');
+    }
+}
