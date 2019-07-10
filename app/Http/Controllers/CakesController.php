659f1810bca9dab4ad16b7c472f56dc5d854249c
Mehran <mehranzolghadr666@gmail.com>
2019-07-10 20:55:03 +0430

    create cake controller created

---
+++ app/Http/Controllers/CakesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use App\Cake;
+
+class CakesController extends Controller
+{
+
+  /**
+  * create cake function
+  * add cake's feilds to Database
+  * request: POST
+  * @param Request
+  * @return Response
+  */
+    public function create(Request $request) {
+
+      $validator = Validator::make($request->all(), [
+        'title' => 'required',
+        'price' => 'required',
+        'main_category' => 'required',
+        'sub_category' => 'required',
+        'weights' => 'required',
+        'number_of_sells' => 'nullable',
+      ]);
+
+      if ($validator->fails()) {
+        return response()->json(['error' => $validator->errors()], 401);
+      }
+
+      $newCake = new Cake;
+      $newCake->name = $request->title;
+      $newCake->price = $request->price;
+      $newCake->main_category = $request->main_category;
+      $newCake->sub_category = $request->sub_category;
+      $newCake->weights = $request->weights;
+      $newCake->admin_id = Auth::guard('admin-api')->user()->id;
+      $newCake->save();
+
+
+      $success['status'] = 'done';
+      return response()->json(['success' => $success], 200);
+
+    }
+
