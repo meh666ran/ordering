879d5977ae06b1bed6f4fd6917d02929bec5a11d
Mehran <mehranzolghadr666@gmail.com>
2019-07-11 13:42:56 +0430

    routes are passing from cors middleware

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,14 @@ use Illuminate\Http\Request;
 */
 
 
-Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.api');
-Route::post('/login', 'Auth\UserLoginController@login')->name('user.login.api');
-Route::post('/register', 'Auth\UserRegisterController@register')->name('user.register.api');
-Route::get('user-details', 'Auth\UserLoginController@details');
-Route::get('admin-details', 'Auth\AdminLoginController@details');
-Route::post('create/cake', 'CakesController@create');
-Route::get('cake/{id}', 'CakesController@show');
-Route::get('/cakes', 'CakesController@productsPage');
-Route::get('/cakes/{order}', 'CakesController@productsPage');
+Route::group(['middleware' => 'cors'], function() {
+  Route::post('/admin/login', 'Auth\AdminLoginController@login');
+  Route::post('/login', 'Auth\UserLoginController@login');
+  Route::post('/register', 'Auth\UserRegisterController@register');
+  Route::get('/user-details', 'Auth\UserLoginController@details');
+  Route::get('/admin-details', 'Auth\AdminLoginController@details');
+  Route::post('/create/cake', 'CakesController@create');
+  Route::get('/cake/{id}', 'CakesController@show');
+  // Route::get('/cakes', 'CakesController@productsPage');
+  Route::get('/cakes/{order}', 'CakesController@productsPage');
+});
