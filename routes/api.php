e22a4d185c50dd26fa5f67f2ecee102f312942b0
mehran <mehranzolghadr666@gmail.com>
2019-07-20 01:45:54 +0430

    group route to make them look better

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,30 @@ use Illuminate\Http\Request;
 
 
 Route::group(['middleware' => 'cors'], function() {
-  Route::post('/admin/login', 'Auth\AdminLoginController@login');
-  Route::post('/login', 'Auth\UserLoginController@login');
-  Route::post('/register', 'Auth\UserRegisterController@register');
-  Route::get('/user-details', 'Auth\UserLoginController@details');
-  Route::get('/admin-details', 'Auth\AdminLoginController@details');
-  Route::post('/create/cake', 'CakesController@create');
-  Route::get('/cake/{id}', 'CakesController@show');
-  Route::get('/cakes/order/{order}', 'CakesController@productsPage');
-  Route::get('/cakes/category/{category}', 'CakesController@showByCategory');
-  Route::post('/create/accessory/', 'AccessoriesController@create');
-  Route::get('/accessories', 'AccessoriesController@showAll');
-  Route::get('/accessories/{id}', 'AccessoriesController@show');
+
+  Route::group(['name' => 'authentication'], function() {
+      Route::post('/admin/login', 'Auth\AdminLoginController@login');
+      Route::post('/register', 'Auth\UserRegisterController@register');
+      Route::post('/login', 'Auth\UserLoginController@login');
+      Route::get('/admin-details', 'Auth\AdminLoginController@details');
+      Route::get('/user-details', 'Auth\UserLoginController@details');
+  });
+
+  Route::group(['name' => 'cakes'], function() {
+    Route::post('/create/cake', 'CakesController@create');
+    Route::get('/cake/{id}', 'CakesController@show');
+    Route::get('/cakes/order/{order}', 'CakesController@productsPage');
+    Route::get('/cakes/category/{category}', 'CakesController@showByCategory');
+    Route::put('/update/cake/{id}', 'CakesController@update');
+    Route::delete('/delete/cake/{id}', 'CakesController@destroy');
+  });
+
+  Route::group(['name' => 'accessories'], function() {
+    Route::post('/create/accessory/', 'AccessoriesController@create');
+    Route::get('/accessories', 'AccessoriesController@showAll');
+    Route::get('/accessories/{id}', 'AccessoriesController@show');
+  });
+
   Route::post('/submit/order', 'OrdersController@create');
+
 });
