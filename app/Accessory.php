caf89789f2f83d5324fafc938882c6a4dc190331
mehran <mehranzolghadr666@gmail.com>
2019-07-21 02:53:21 +0430

    add fillable to accessory model

--- app/Accessory.php
+++ app/Accessory.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accessory extends Model
 {
+
+  protected $fillable = [
+      'title', 'price', 'category', 'admin_id', 
+  ];
+
   public function admin() {
     return $this->belongsTo('App\Admin');
   }
